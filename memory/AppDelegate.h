d5d1c120e9b248e2c4d4a54d020057a02ac73e85
Admin <Admin@Admins-Mac.local>
2014-01-13 20:47:04 +0200

    Initial Commit

---
+++ memory/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  memory
+//
+//  Created by Admin on 1/13/14.
+//  Copyright (c) 2014 msu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
